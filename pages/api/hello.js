98fe38c8ce6a45ace13748d340efd8b3e6decaa2
waqas837 <73779175+waqas837@users.noreply.github.com>
2021-08-24 21:57:51 +0500

    new one

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,7 +1,6 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import db from "../../utils/db";
 export default async function handler(req, res) {
-  // await db.connect();
-  // await db.disconnect();
+  await db.connect();
   res.status(200).json({ name: "John Doe" });
 }
